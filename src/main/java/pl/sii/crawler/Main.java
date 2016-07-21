f87684d269041b50ea9c7be99796305ea212ecee
Kuras <przemyk007@interia.eu>
2016-07-21 19:58:38 +0200

    add rollback

--- src/main/java/pl/sii/crawler/Main.java
+++ src/main/java/pl/sii/crawler/Main.java
@@ -19,25 +19,32 @@ public class Main {
     public static void main(String[] args) throws IOException, SolrServerException {
 
         System.out.println("Start index");
-        HttpSolrClient server = SiiSolrServer.getSolrClient("wsi");
+        HttpSolrClient server = null;
+        try {
+            server = SiiSolrServer.getSolrClient("wsi");
 
-        IntStream.iterate( 2, i -> i + 1 )
-                .map( Crawler::addPage )
-                .limit( 162 )
-                .reduce( 0 , (p, n) -> p + n );
+            IntStream.iterate(163, i -> i + 1)
+                    .map(Crawler::addPage)
+                    .limit(325)
+                    .reduce(0, (p, n) -> p + n);
 
-        Collection<SolrInputDocument> docs = Crawler.getStreamOfPages()
-                .map( page -> {
-                    SolrInputDocument doc = new SolrInputDocument();
-                    doc.addField( "page", page.getNumber(), 1.0f );
-                    doc.addField( "content", page.getContent() );
-                    return doc;
-                } )
-                .collect( Collectors.toList() );
+            Collection<SolrInputDocument> docs = Crawler.getStreamOfPages()
+                    .map(page -> {
+                        SolrInputDocument doc = new SolrInputDocument();
+                        doc.addField("page", page.getNumber(), 1.0f);
+                        doc.addField("content", page.getContent());
+                        return doc;
+                    })
+                    .collect(Collectors.toList());
 
-        server.add( docs );
-        server.commit();
-        server.close();
+            server.add(docs);
+            server.commit();
+        } catch (RuntimeException re) {
+            server.rollback();
+            System.out.println( "Huston we've a problem and rollback all buch of current docs");
+        } finally {
+            server.close();
+        }
         System.out.println("End index");
     }
 
--- src/main/java/pl/sii/solr/Tests.java
+++ src/main/java/pl/sii/solr/Tests.java
@@ -25,7 +25,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
